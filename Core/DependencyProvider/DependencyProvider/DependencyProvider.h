5129fb6a60a499509347210c8f4f641b846d3147
Evan Anger <devandanger@gmail.com>
2021-05-27 02:16:50 -0400

    Added deps

---
+++ Core/DependencyProvider/DependencyProvider/DependencyProvider.h
@@ -0,0 +1,18 @@
+//
+//  DependencyProvider.h
+//  DependencyProvider
+//
+//  Created by Evan Anger on 5/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyProvider.
+FOUNDATION_EXPORT double DependencyProviderVersionNumber;
+
+//! Project version string for DependencyProvider.
+FOUNDATION_EXPORT const unsigned char DependencyProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyProvider/PublicHeader.h>
+
+
